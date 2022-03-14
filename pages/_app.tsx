480f9b4e9d2acc9238410ef408f076aa5a976547
Matthijs <matthijsmud@gmail.com>
2022-03-14 17:50:26 +0100

    Extract "emotion" style tags from the body.
    Valid HTML only has `<style>` in the `<head>`, which is incorrectly
    rendered by default. Hydrating the code would place the tags in
    the correct spot if left alone, though that is not ideal.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// https://github.com/mui/material-ui/blob/78c06894885cc607309733d5ba3a769c67113993/examples/nextjs/pages/_app.js
+import type { AppProps } from 'next/app';
+import { CacheProvider } from "@emotion/react";
+import createCache, { EmotionCache } from "@emotion/cache";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const clientCache = createCache({ key: "css", prepend: true });
+
+function MyApp(props: AppProps & { emotionCache: EmotionCache }) {
+  const { Component, emotionCache = clientCache, pageProps } = props;
+  return <CacheProvider value={emotionCache}>
+    <Component {...pageProps} />
+  </CacheProvider>
 }
 
 export default MyApp
---
+++ pages/_document.tsx
@@ -0,0 +1,50 @@
+// https://github.com/mui/material-ui/blob/78c06894885cc607309733d5ba3a769c67113993/examples/nextjs/pages/_document.js
+import Document, { DocumentContext, DocumentInitialProps, Html, Head, Main, NextScript, } from "next/document";
+import { CacheProvider } from "@emotion/react";
+import createEmotionServer from "@emotion/server/create-instance";
+import createCache, { EmotionCache } from "@emotion/cache";
+
+export default class ExtractionDocument extends Document<{ emotionStyleTags: any }> {
+
+  render() {
+    return <Html lang="en">
+      <Head>
+        {this.props.emotionStyleTags}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  }
+
+
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps & { emotionStyleTags: any }> {
+    const cache = createCache({ key: "css", prepend: true });
+    const { extractCriticalToChunks } = createEmotionServer(cache);
+
+    const renderPage = ctx.renderPage;
+    ctx.renderPage = () => {
+      return renderPage({
+        enhanceApp: App => function EnhanceApp(props) {
+          return <App emotionCache={cache} {...props} />;
+        }
+      });
+    };
+
+    const initialProps = await Document.getInitialProps(ctx);
+    const emotionStyles = extractCriticalToChunks(initialProps.html);
+    const emotionStyleTags = emotionStyles.styles.map((style) => {
+      return <style 
+        data-emotion={`${style.key} ${style.ids.join(" ")}`}
+        key={style.key}
+        dangerouslySetInnerHTML={{ __html: style.css }}
+      />
+    });
+
+    return {
+      ...initialProps,
+      emotionStyleTags,
+    }
+  }
+}
