959fb9609e104ab5d265d910f997cc9d1e022cc0
Matthijs <matthijsmud@gmail.com>
2022-03-22 15:49:33 +0100

    Configure next to use a different basepath.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withPreact = require("next-plugin-preact");
 /** @type {import('next').NextConfig} */
 const nextConfig = withPreact({
   reactStrictMode: true,
+  basePath: "/portfolio",
 });
 
 module.exports = nextConfig
