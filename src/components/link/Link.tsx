d1c3139875f6dd275c37aae300ea0ea316c09bb4
Matthijs <matthijsmud@gmail.com>
2022-04-27 00:34:39 +0200

    Link component handling outgoing links.
    Next links should only be used for internal links; externals typically don't provide the required data (partially due to CORS).
    
    The markdown specification does however not distinguish between the two.

---
+++ src/components/link/Link.tsx
@@ -0,0 +1,33 @@
+import type { FC } from "react";
+import NextLink, { LinkProps } from "next/link";
+import styled from "@emotion/styled";
+
+// Technically not necessarily external, but specifying 
+// a protocol is not required for internal links.
+const isExternal = /^http/;
+
+const A = styled("a")({
+  textDecoration: "none",
+  // TODO: retrieve styling from the theme.
+  color: "darkturquoise",
+  "&:hover": {
+    color: "teal",
+  }
+});
+
+/**
+ * Uniform link component for linking to both internal
+ * as external links. 
+ */
+export const Link: FC<LinkProps> = (props) => {
+  const { children, href, ...link } = props; 
+
+  if (typeof href === "string" && isExternal.test(href)) {
+    return <A href={href}>{children}</A>
+  }
+  return <NextLink {...link} href={href} passHref>
+    <A>
+      {children}
+    </A>
+  </NextLink>
+}
