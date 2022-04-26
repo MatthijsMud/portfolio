07353a85a732be1959f19b017addf9c1c5359060
Matthijs <matthijsmud@gmail.com>
2022-04-27 00:29:56 +0200

    Use the database to get the markdown file.

--- src/content/getBySlug.ts
+++ src/content/getBySlug.ts
@@ -1,24 +1,10 @@
-import { readdir, readFile } from "fs/promises";
-import { join, sep, } from "path";
-
-const normalizePath = (path: string): string => {
-  // The web uses forward slashes, but not all operating systems agree.
-  // Last value tends to be a file, but… eh.
-  const folders = path.split("/");
-  folders.forEach(relative => {
-  // Paths ought to be specified relative to the root of the project.
-  // Any files outside of this root should not be read; this means
-  // that there is no reason to go "up" a folder.
-    if (relative === "..") {
-      throw new Error("Slug includes reference to parent folder.");
-    }
-  });
-  return join.apply(undefined, ["content", ...folders]);
-}
+import { db } from "./database";
 
 export const getBySlug = async (slug: string) => {
-  const path = join(process.cwd(), normalizePath(slug));
-
-  const fileContents = await readFile(path, "utf-8");
-  return fileContents;
+  const matches = (await db).markdown.filter(md => {
+    return md.metadata["slug"] === slug;
+  });
+  if (matches.length > 0) {
+    return matches[0];
+  }
 }
