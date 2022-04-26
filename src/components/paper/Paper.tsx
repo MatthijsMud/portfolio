f3ff3f08555f49271b2668d12df5c7d4a25b63e2
Matthijs <matthijsmud@gmail.com>
2022-04-27 01:05:15 +0200

    Paper component used as background.

---
+++ src/components/paper/Paper.tsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const Paper = styled("div")(({ theme }) => {
+  return {
+    backgroundColor: "#333",
+    borderRadius: "0.25rem",
+    boxShadow: "0 1px 0.5rem #0008",
+    overflow: "hidden",
+
+  }
+});
