548934311c181e0851586aadc7bfc77616c878bc
Matthijs <matthijsmud@gmail.com>
2022-04-26 22:59:32 +0200

    Index all content into a database for easy acces.

---
+++ src/content/database.ts
@@ -0,0 +1,44 @@
+import { readdir, readFile, stat } from "fs/promises";
+import { from, share, lastValueFrom } from "rxjs";
+import { tap, mergeMap, filter, toArray } from "rxjs/operators";
+import { join } from "path";
+
+const isHidden = (path: string): boolean => /^\./.test(path);
+
+async function* files(root: string) {
+  const stack = [root];
+  while (stack.length) {
+    const dir = stack.pop();
+    if (typeof dir !== "string") continue;
+
+    for (const fsi of await readdir(dir, { withFileTypes: true })) {
+      // Files starting with a period ('.') are hidden by convention.
+      // These folders tend to include files that are not to be used
+      // as content, and should thus be ignored.
+      if (isHidden(fsi.name)) continue;
+
+      const path = join(dir, fsi.name);
+
+      if (fsi.isDirectory()) {
+        stack.push(path);
+      } else {
+        yield path;
+      }
+    }
+  }
+}
+
+export const db = (async () => {
+  console.log("Creating database");
+  const index = from(files(join(process.cwd(), "content"))).pipe(
+    share(),
+  );
+  const markdown = lastValueFrom(index.pipe(
+    filter(path => /\.md$/.test(path)),
+    mergeMap(path => readFile(path, { encoding: "utf-8"})),
+
+    toArray(),
+  ));
+  const a = await Promise.all([markdown, ]);
+  return {  };
+})();
